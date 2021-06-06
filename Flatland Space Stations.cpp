76daf36d05222580d1f3ed4bc42b3de155b17995
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-06-06 21:12:35 +0900

    Update Flatland Space Stations.cpp

--- Flatland Space Stations.cpp
+++ Flatland Space Stations.cpp
@@ -3,22 +3,16 @@ int flatlandSpaceStations(int n, vector<int> arr)
     std::sort(arr.begin(), arr.end());
     int maxDistance = arr[0];
 
-    for (size_t i = 1; i < arr.size(); ++i)
+    for (int i = 1; i < arr.size(); i++)
     {
         int distance = (arr[i] - arr[i-1]) / 2;
-
         if (maxDistance < distance)
         {
             maxDistance = distance;
         }
     }
 
-    int lastGap = (n-1) - arr[arr.size()-1];
+    int lastGap = (n-1) - arr[arr.size() - 1];
 
-    if (maxDistance < lastGap)
-    {
-        maxDistance = lastGap;
-    }
-
-    return maxDistance;
+    return (maxDistance < lastGap) ? lastGap : maxDistance;
 }
