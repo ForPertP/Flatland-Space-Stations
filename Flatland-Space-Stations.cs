b76d71177b030eeb4c54925f73b975a13bed9cea
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-06-28 16:33:13 +0900

    Update Flatland-Space-Stations.cs

--- Flatland-Space-Stations.cs
+++ Flatland-Space-Stations.cs
@@ -12,4 +12,24 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
-class Solution {}
+class Solution {
+ // Complete the flatlandSpaceStations function below.
+    static int flatlandSpaceStations(int n, int[] arr) {
+        Array.Sort(arr);
+        int maxDistance = arr[0];
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            int distance = (arr[i] - arr[i - 1]) / 2;
+            if (maxDistance < distance)
+            {
+                maxDistance = distance;
+            }
+        }
+
+        int lastGap = (n - 1) - arr[arr.Length - 1];
+
+        return (maxDistance < lastGap) ? lastGap : maxDistance;
+
+    }  
+}
