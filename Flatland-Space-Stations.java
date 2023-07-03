c44ed8a3e971714d61124a306ce9a1c67a7d1b96
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-07-03 16:37:35 +0900

    Update Flatland-Space-Stations.java

--- Flatland-Space-Stations.java
+++ Flatland-Space-Stations.java
@@ -5,3 +5,24 @@ import java.text.*;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the flatlandSpaceStations function below.
+    static int flatlandSpaceStations(int n, int[] arr) {
+        Arrays.sort(arr);
+        int maxDistance = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            int distance = (arr[i] - arr[i - 1]) / 2;
+            if (maxDistance < distance) {
+                maxDistance = distance;
+            }
+        }
+
+        int lastGap = (n - 1) - arr[arr.length - 1];
+
+        return (maxDistance < lastGap) ? lastGap : maxDistance;
+
+    }
+}
