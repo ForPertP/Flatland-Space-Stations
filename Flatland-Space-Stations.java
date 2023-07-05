6925c15446a82df827a65b7b5ecf3902e03b2a13
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-07-05 16:26:51 +0900

    Update Flatland-Space-Stations.java

--- Flatland-Space-Stations.java
+++ Flatland-Space-Stations.java
@@ -25,4 +25,35 @@ public class Solution {
         return (maxDistance < lastGap) ? lastGap : maxDistance;
 
     }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0]);
+
+        int m = Integer.parseInt(nm[1]);
+
+        int[] c = new int[m];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int result = flatlandSpaceStations(n, c);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
